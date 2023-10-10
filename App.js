d96a3d3dc550a09d6cf44b613a6b94109321dd57
Ryan Paige <universallyry@gmail.com>
2023-10-10 15:32:38 -0400

    small setup

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';  
+
+function HomeScreen() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{ flex: 1, paddingTop: insets.top, ...styles.container }}>
+      <Text style={{ fontSize: 28 }}>Content is in safe area.</Text>
+    </View>
+  );
+};
 
 export default function App() {
+  
+  let [fontsLoaded, fontError] = useFonts({
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <HomeScreen onLayout={onLayoutRootView}/>
+        <StatusBar style="auto" />
+    </SafeAreaProvider>
   );
 }
 
